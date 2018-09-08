1e9ca7fa552a9d93f9f45e203e0a7f1850dcd456
Bear Tion frey <btion-fr@e4r10p12.21-school.ru>
2018-09-08 20:26:36 +0300

    go

--- evalexp/ex00/evalexpr.h
+++ evalexp/ex00/evalexpr.h
@@ -6,7 +6,7 @@
 /*   By: btion-fr <btion-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 18:46:44 by btion-fr          #+#    #+#             */
-/*   Updated: 2018/09/08 19:12:05 by btion-fr         ###   ########.fr       */
+/*   Updated: 2018/09/08 20:20:15 by btion-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 # define EVALEXPR_H
 
 # include <unistd.h>
+# include <stdio.h>
 
 char	*g_stack;
 int		ft_atoi(char *str);
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
 void	ft_putstr(char *str);
+char	*ft_strcat(char *dest, char *src);
 
 #endif
--- evalexp/ex00/main.c
+++ evalexp/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: btion-fr <btion-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 18:45:20 by btion-fr          #+#    #+#             */
-/*   Updated: 2018/09/08 19:52:35 by btion-fr         ###   ########.fr       */
+/*   Updated: 2018/09/08 20:26:13 by btion-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 
 int		eval_expr(char *str)
 {
-	int result;
+	int		result;
 
 	result = 42;
 	return (42);
+}
+
+void	ft_poezd(char c, char *texas, char *california)
+{
+	if (c >= '0' && c <= '9')
+		ft_strcat(california, &c);
+}
+
+void	to_pn(char *str)
+{
+	int		cursor;
+	char	*texas;
+	char	*california;
+
+	cursor = 0;
+	while (str[cursor] != '\0')
+		ft_poezd(str[cursor], texas, california);
+	printf("%s", california);
 }
 
 int		main(int ac, char **av)
@@ -25,7 +43,7 @@ int		main(int ac, char **av)
 	av = 0;
 	if (ac > 1)
 	{
-		//ft_putnbr(eval_expr(av[1]));
+		to_pn(av[1]);
 		ft_putchar('\n');
 	}
 	return (0);
