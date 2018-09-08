041e2139a5f5fef617c51a8e03491c9925b0439d
Bear Tion frey <btion-fr@e4r10p12.21-school.ru>
2018-09-08 21:11:06 +0300

    go

--- evalexp/ex00/main.c
+++ evalexp/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: btion-fr <btion-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 18:45:20 by btion-fr          #+#    #+#             */
-/*   Updated: 2018/09/08 20:26:13 by btion-fr         ###   ########.fr       */
+/*   Updated: 2018/09/08 21:05:28 by btion-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		eval_expr(char *str)
 void	ft_poezd(char c, char *texas, char *california)
 {
 	if (c >= '0' && c <= '9')
-		ft_strcat(california, &c);
+		ft_strcat(california, "1");
 }
 
 void	to_pn(char *str)
@@ -34,16 +34,15 @@ void	to_pn(char *str)
 
 	cursor = 0;
 	while (str[cursor] != '\0')
-		ft_poezd(str[cursor], texas, california);
+		ft_poezd(str[cursor++], texas, california);
 	printf("%s", california);
 }
 
-int		main(int ac, char **av)
+int		main(int argc, char **argv)
 {
-	av = 0;
-	if (ac > 1)
+	if (argc > 1)
 	{
-		to_pn(av[1]);
+		to_pn(argv[1]);
 		ft_putchar('\n');
 	}
 	return (0);
